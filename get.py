837d732625bc566867932ab4030e791452c5ed14
Marc Landolt <mail@marclandolt.ch>
2017-09-02 16:25:10 +0200

    Update get.py

--- get.py
+++ get.py
@@ -11,10 +11,22 @@ url3 = 'URL?s=1,0'                                                         #URL3
 
 payload = {'make': 'send','user': 'BENUTZERNAME','pass': 'PASSWORT'}       #Benutzername und Passwort
 
-response = requests.post(url, data=payload)                                #übermittle den Payload mit dem Benutzer und
+                                                                           #übermittle den Payload mit dem Benutzer und
                                                                            #passwort und empfange die Server-Antwort
-#print(response.text)                                                      #Drucke die Server Antwort
-#print(response.status_code)
+response1 = requests.post(url1, data=payload, headers={'Connection':'close'})
+response2 = requests.post(url2, data=payload, headers={'Connection':'close'})
+response3 = requests.post(url3, data=payload, headers={'Connection':'close'})
+
+print(response1.text)                                                      #Drucke die Server Antworten
+#print(response1.status_code)
+
+print(response2.text)
+#print(response2.status_code)
+
+print(response3.text)
+#print(response3.status_code)
+
+
 
 for line in response:                                                      #für jede zeile der Serverantwort mache:
 	if re.search('charts', line):                                      #suche nach dem Wort charts
@@ -23,9 +35,6 @@ for line in response:                                                      #für
 			print line.split(",")[11].split("]")[0]            #trenne die Zeile bei den kommas und nimm
 			                                                   #die 11 spalte, trenne davon beim ] und nimm die
 				                                           #erste spalte
-
-response2 = requests.post(url2, data=payload)                              #übermittle den Payload mit dem Benutzer und					
-#print(response2.text)                                                     #Drucke die Server Antwort2
 
 # Beispiel einer Zeile:
 #  charts[1]['mittel'] = [['Sa',22.4],['So',20.9],['Mo',21.5],['Di',21.7],['Mi',22.3],['Do',16.8],['Fr',13.9]];
