8b024888bb91dcf57c9fe1762832fcd5f4111de8
Marc Landolt <mail@marclandolt.ch>
2017-09-03 19:33:00 +0200

    Update get.py

--- get.py
+++ get.py
@@ -16,7 +16,7 @@ url2 = url1+'?s=1,0'
 url3 = url1+'?s=1,1'
 
 #Benutzername und passwort
-payload = {'make': 'send','user': 'USERNAME','pass': 'PASSWORD'}
+payload = {'make': 'send','user': 'USER','pass': 'PASSWORD'}
 
 #Aktuelle Stunde als Zeitstempel, da sich die Heizung träge verhält macht es keinen Sinn mehr als 1x die Stunde abzufragen
 currentHour = time.strftime("%Y%m%d%H")
@@ -27,18 +27,18 @@ filename = './'+currentHour+'.dump'
 #Eingabeparameter verarbeiten
 # Instantiate the parser
 def parseArgs():
-	parser = argparse.ArgumentParser(description='Liest Werte aus Stiebel Elron Webinterface aus')
+	parser = argparse.ArgumentParser(description='Liest Werte aus Stiebel Elron Webinterface aus aufruf z.B: $ python get.py --live "PUFFERISTTEMPERATUR"')
 	parser.add_argument('--csv', action="store_true", default=False, help='gibt alle Daten als CSV aus')
 	parser.add_argument('--middle', action="store", dest="middle", help='gibt die Mittelwerte von der Startseite aus')
 	parser.add_argument('--live', action="store", dest="specific", help='gibt die Live Werte aus')
 	args = parser.parse_args()
-	print ("csv: %s", args.csv)
-
+	#print ("csv: %s", args.csv)
+	#
 	if not (args.middle is None):
-		print ("middle: %s", args.middle)
+	#	print ("middle: %s", args.middle)
 		evaluateMiddle(args.middle)
 	if not (args.specific is None):
-		print ("Spezifisch: %s", args.specific)
+	#	print ("Spezifisch: %s", args.specific)
 		evaluateLive(args.specific)
 	
 
@@ -70,13 +70,22 @@ def printResponse():
 def evaluateMiddle(value):
 	with open(filename, 'r') as fin:
 		for line in fin:
-			if re.search('charts', line):#
+			if re.search('charts', line):
 				##print line
 				if re.search('mittel', line):
 					print line.split(",")[11].split("]")[0]
 
 def evaluateLive(value):
-	print "test"
+	#print value
+	with open(filename, 'r') as fin:
+		for line in fin:
+			if re.search(">"+value+"<", line):
+				
+				print fin.next().split(">")[1].split(" ")[0];
+				#if re.search('mittel', line):
+				#	print line.split(",")[11].split("]")[0]
+
+
 
 #Hole Wert mit dem namen der in der Variabel value gespeichert ist
 def getValue(value):
